6964f9d787eb08a25d67a62eea5c482c593ab3b4
Elijah Mbugi <mukundielijah@gmail.com>
2022-07-25 08:05:22 +0300

    completed

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,5 +4,19 @@ class MoviesController < ApplicationController
     movies = Movie.all
     render json: movies
   end
+  
+  def create
+    movie = Movie.create(movie_params)
+    render json: movie, status: :created
+  # rescue ActiveRecord::RecordInvalid => e
+  #   render json: { errors: e.record.errors }, status: :unprocessable_entity
+  byebug
+  end
+
+  private
+
+  def movie_params
+    params..permit(:title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director)
+  end
 
 end
